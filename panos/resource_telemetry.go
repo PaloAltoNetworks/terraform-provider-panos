ec19e85bddbf2c37bd7ece866bbc9dff48c1b497
Garfield Freeman <gfreeman@paloaltonetworks.com>
2018-06-18 06:59:35 -0700

    adding resource: panos_telemetry

---
+++ panos/resource_telemetry.go
@@ -0,0 +1,137 @@
+package panos
+
+import (
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/dev/telemetry"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceTelemetry() *schema.Resource {
+	return &schema.Resource{
+		Create: createTelemetry,
+		Read:   readTelemetry,
+		Update: updateTelemetry,
+		Delete: deleteTelemetry,
+
+		Schema: map[string]*schema.Schema{
+			"application_reports": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"threat_prevention_reports": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"url_reports": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"file_type_identification_reports": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"threat_prevention_data": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"threat_prevention_packet_captures": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"product_usage_stats": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"passive_dns_monitoring": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func parseTelemetry(d *schema.ResourceData) telemetry.Settings {
+	o := telemetry.Settings{
+		ApplicationReports:             d.Get("application_reports").(bool),
+		ThreatPreventionReports:        d.Get("threat_prevention_reports").(bool),
+		UrlReports:                     d.Get("url_reports").(bool),
+		FileTypeIdentificationReports:  d.Get("file_type_identification_reports").(bool),
+		ThreatPreventionData:           d.Get("threat_prevention_data").(bool),
+		ThreatPreventionPacketCaptures: d.Get("threat_prevention_packet_captures").(bool),
+		ProductUsageStats:              d.Get("product_usage_stats").(bool),
+		PassiveDnsMonitoring:           d.Get("passive_dns_monitoring").(bool),
+	}
+
+	return o
+}
+
+func createTelemetry(d *schema.ResourceData, meta interface{}) error {
+	fw := meta.(*pango.Firewall)
+	o := parseTelemetry(d)
+
+	if err := fw.Device.Telemetry.Set(o); err != nil {
+		return err
+	}
+
+	d.SetId(fw.Hostname)
+	return readTelemetry(d, meta)
+}
+
+func readTelemetry(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	fw := meta.(*pango.Firewall)
+	o, err := fw.Device.Telemetry.Get()
+	if err != nil {
+		e2, ok := err.(pango.PanosError)
+		if ok && e2.ObjectNotFound() {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	d.Set("application_reports", o.ApplicationReports)
+	d.Set("threat_prevention_reports", o.ThreatPreventionReports)
+	d.Set("url_reports", o.UrlReports)
+	d.Set("file_type_identification_reports", o.FileTypeIdentificationReports)
+	d.Set("threat_prevention_data", o.ThreatPreventionData)
+	d.Set("threat_prevention_packet_captures", o.ThreatPreventionPacketCaptures)
+	d.Set("product_usage_stats", o.ProductUsageStats)
+	d.Set("passive_dns_monitoring", o.PassiveDnsMonitoring)
+
+	return nil
+}
+
+func updateTelemetry(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	fw := meta.(*pango.Firewall)
+	o := parseTelemetry(d)
+
+	lo, err := fw.Device.Telemetry.Get()
+	if err != nil {
+		return err
+	}
+	lo.Copy(o)
+	if err = fw.Device.Telemetry.Edit(lo); err != nil {
+		return err
+	}
+
+	return readTelemetry(d, meta)
+}
+
+func deleteTelemetry(d *schema.ResourceData, meta interface{}) error {
+	fw := meta.(*pango.Firewall)
+
+	err := fw.Device.Telemetry.Delete()
+	if err != nil {
+		e2, ok := err.(pango.PanosError)
+		if !ok || !e2.ObjectNotFound() {
+			return err
+		}
+	}
+	d.SetId("")
+	return nil
+}
---
+++ website/docs/r/telemetry.html.markdown
@@ -0,0 +1,46 @@
+---
+layout: "panos"
+page_title: "panos: panos_telemetry"
+sidebar_current: "docs-panos-resource-telemetry"
+description: |-
+  Manages anonymous statistics sharing.
+---
+
+# panos_telemetry
+
+This resource allows you to add/update/delete telemetry sharing.
+
+Join other Palo Alto Networks customers in a global sharing community, helping
+to raise the bar against the latest attack techniques. Your participation
+allows us to deliver new threat prevention controls across the attack
+lifecycle. Choose the type of data you share across applications, threat
+intelligence, and device health information to improve the fidelity of the
+protections we deliver. This is an opt-in feature controlled with granular
+policy, and we encourage you to join the community.
+
+
+## Example Usage
+
+```hcl
+resource "panos_telemetry" "example" {
+    threat_prevention_reports = true
+    threat_prevention_data = true
+    threat_prevention_packet_captures = true
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `application_reports` - (Bool, optional) Application reports.
+* `threat_prevention_reports` - (Bool, optional) Threat reports.
+* `url_reports` - (Bool, optional) URL reports.
+* `file_type_identification_reports` - (Bool, optional) File type identification
+  reports.
+* `threat_prevention_data` - (Bool, optional) Threat prevention data.
+* `threat_prevention_packet_captures` - (Bool, optional) Enable sending packet-
+  captures with threat prevention information. This requires that
+  `threat_prevention_data` also be enabled.
+* `product_usage_stats` - (Bool, optional) Health and performance reports.
+* `passive_dns_monitoring` - (Bool, optional) Passive DNS monitoring.
