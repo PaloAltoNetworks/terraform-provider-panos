188e894540d815e15f1df457f61d054da58dbdae
Garfield Freeman <gfreeman@paloaltonetworks.com>
2018-08-27 09:22:13 -0700

    Fixes #64 - Panorama virtual routers

---
+++ panos/resource_panorama_virtual_router.go
@@ -0,0 +1,232 @@
+package panos
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/netw/routing/router"
+	"github.com/PaloAltoNetworks/pango/util"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourcePanoramaVirtualRouter() *schema.Resource {
+	return &schema.Resource{
+		Create: createPanoramaVirtualRouter,
+		Read:   readPanoramaVirtualRouter,
+		Update: updatePanoramaVirtualRouter,
+		Delete: deletePanoramaVirtualRouter,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"template": &schema.Schema{
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"template_stack"},
+			},
+			"template_stack": &schema.Schema{
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"template"},
+			},
+			"vsys": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "vsys1",
+				ForceNew: true,
+			},
+			"interfaces": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"static_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      10,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"static_ipv6_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      10,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"ospf_int_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      30,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"ospf_ext_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      110,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"ospfv3_int_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      30,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"ospfv3_ext_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      110,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"ibgp_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      200,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"ebgp_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      20,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+			"rip_dist": &schema.Schema{
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      120,
+				ValidateFunc: validateIntInRange(10, 240),
+			},
+		},
+	}
+}
+
+func parsePanoramaVirtualRouterId(v string) (string, string, string, string) {
+	t := strings.Split(v, IdSeparator)
+	return t[0], t[1], t[2], t[3]
+}
+
+func buildPanoramaVirtualRouterId(a, b, c, d string) string {
+	return strings.Join([]string{a, b, c, d}, IdSeparator)
+}
+
+func parsePanoramaVirtualRouter(d *schema.ResourceData) (string, string, string, router.Entry) {
+	tmpl := d.Get("template").(string)
+	ts := d.Get("template_stack").(string)
+	vsys := d.Get("vsys").(string)
+
+	o := router.Entry{
+		Name:           d.Get("name").(string),
+		Interfaces:     asStringList(d.Get("interfaces").([]interface{})),
+		StaticDist:     d.Get("static_dist").(int),
+		StaticIpv6Dist: d.Get("static_ipv6_dist").(int),
+		OspfIntDist:    d.Get("ospf_int_dist").(int),
+		OspfExtDist:    d.Get("ospf_ext_dist").(int),
+		Ospfv3IntDist:  d.Get("ospfv3_int_dist").(int),
+		Ospfv3ExtDist:  d.Get("ospfv3_ext_dist").(int),
+		IbgpDist:       d.Get("ibgp_dist").(int),
+		EbgpDist:       d.Get("ebgp_dist").(int),
+		RipDist:        d.Get("rip_dist").(int),
+	}
+
+	return tmpl, ts, vsys, o
+}
+
+func createPanoramaVirtualRouter(d *schema.ResourceData, meta interface{}) error {
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vsys, o := parsePanoramaVirtualRouter(d)
+
+	if err := pano.Network.VirtualRouter.Set(tmpl, ts, vsys, o); err != nil {
+		return err
+	}
+
+	d.SetId(buildPanoramaVirtualRouterId(tmpl, ts, vsys, o.Name))
+	return readPanoramaVirtualRouter(d, meta)
+}
+
+func readPanoramaVirtualRouter(d *schema.ResourceData, meta interface{}) error {
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vsys, name := parsePanoramaVirtualRouterId(d.Id())
+
+	o, err := pano.Network.VirtualRouter.Get(tmpl, ts, name)
+	if err != nil {
+		e2, ok := err.(pango.PanosError)
+		if ok && e2.ObjectNotFound() {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+	rv, err := pano.IsImported(util.VirtualRouterImport, tmpl, ts, vsys, name)
+	if err != nil {
+		return err
+	}
+
+	d.Set("template", tmpl)
+	d.Set("template_stack", ts)
+	d.Set("name", o.Name)
+	if rv {
+		d.Set("vsys", vsys)
+	} else {
+		d.Set("vsys", fmt.Sprintf("(not %s)", vsys))
+	}
+	if err := d.Set("interfaces", o.Interfaces); err != nil {
+		log.Printf("[WARN] Error setting 'interfaces' for %q: %s", d.Id(), err)
+	}
+	d.Set("static_dist", o.StaticDist)
+	d.Set("static_ipv6_dist", o.StaticIpv6Dist)
+	d.Set("ospf_int_dist", o.OspfIntDist)
+	d.Set("ospf_ext_dist", o.OspfExtDist)
+	d.Set("ospfv3_int_dist", o.Ospfv3IntDist)
+	d.Set("ospfv3_ext_dist", o.Ospfv3ExtDist)
+	d.Set("ibgp_dist", o.IbgpDist)
+	d.Set("ebgp_dist", o.EbgpDist)
+	d.Set("rip_dist", o.RipDist)
+
+	return nil
+}
+
+func updatePanoramaVirtualRouter(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vsys, o := parsePanoramaVirtualRouter(d)
+
+	lo, err := pano.Network.VirtualRouter.Get(tmpl, ts, o.Name)
+	if err != nil {
+		return err
+	}
+	lo.Copy(o)
+	if err = pano.Network.VirtualRouter.Edit(tmpl, ts, vsys, lo); err != nil {
+		return err
+	}
+
+	return readPanoramaVirtualRouter(d, meta)
+}
+
+func deletePanoramaVirtualRouter(d *schema.ResourceData, meta interface{}) error {
+	var err error
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, _, name := parsePanoramaVirtualRouterId(d.Id())
+
+	if name == "default" {
+		err = pano.Network.VirtualRouter.CleanupDefault(tmpl, ts)
+	} else {
+		err = pano.Network.VirtualRouter.Delete(tmpl, ts, name)
+	}
+	if err != nil {
+		e2, ok := err.(pango.PanosError)
+		if !ok || !e2.ObjectNotFound() {
+			return err
+		}
+	}
+	d.SetId("")
+	return nil
+}
---
+++ website/docs/r/panorama_virtual_router.html.markdown
@@ -0,0 +1,55 @@
+---
+layout: "panos"
+page_title: "panos: panos_panorama_virtual_router"
+sidebar_current: "docs-panos-panorama-resource-virtual-router"
+description: |-
+  Manages Panorama virtual routers.
+---
+
+# panos_panorama_virtual_router
+
+This resource allows you to add/update/delete Panorama virtual routers
+for both templates and template statcks.
+
+**Note** - The `default` virtual router may be configured with this resource,
+however it will not be deleted from the firewall.  It will only be unexported
+from the vsys that it is currently imported in, and any interfaces imported
+into the virtual router will be removed.
+
+## Example Usage
+
+```hcl
+# Configure a bare-bones ethernet interface.
+resource "panos_panorama_virtual_router" "example" {
+    name = "my virtual router"
+    template = "foo"
+    static_dist = 15
+    interfaces = ["ethernet1/1", "ethernet1/2"]
+}
+```
+
+## Argument Reference
+
+One and only one of the following must be specified:
+
+* `template` - The template name.
+* `template_stack` - The template stack name.
+
+The following arguments are supported:
+
+* `name` - (Required) The virtual router's name.
+* `vsys` - (Required) The vsys that will use this virtual router.  This should
+  be something like `vsys1` or `vsys3`.
+* `interfaces` - (Optional) List of interfaces that should use this virtual
+  router.
+* `static_dist` - (Optional) Admin distance - Static (default: `10`).
+* `static_ipv6_dist` - (Optional) Admin distance - Static IPv6 (default: `10`).
+* `ospf_int_dist` - (Optional) Admin distance - OSPF Int (default: `30`).
+* `ospf_ext_dist` - (Optional) Admin distance - OSPF Ext (default: `110`).
+* `ospfv3_int_dist` - (Optional) Admin distance - OSPFv3 Int (default:
+  `30`).
+* `ospfv3_ext_dist` - (Optional) Admin distance - OSPFv3 Ext (default:
+  `110`).
+* `ibgp_dist` - (Optional) Admin distance - IBGP (default: `200`).
+* `ebgp_dist` - (Optional) Admin distance - EBGP (default: `20`).
+* `rip_dist` - (Optional) Admin distance - RIP (default: `120`).
