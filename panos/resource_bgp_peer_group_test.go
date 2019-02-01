9c094eefd893a58cebc1f8d9b41c1a95b2f30473
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-02-01 14:28:04 -0800

    Fixes #114 - bgp peer groups

---
+++ panos/resource_bgp_peer_group_test.go
@@ -0,0 +1,165 @@
+package panos
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/netw/routing/protocol/bgp/peer/group"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccPanosBgpPeerGroup_basic(t *testing.T) {
+	if !testAccIsFirewall {
+		t.Skip(SkipFirewallAccTest)
+	}
+
+	var o group.Entry
+	vr := fmt.Sprintf("tf%s", acctest.RandString(6))
+	name := fmt.Sprintf("tf%s", acctest.RandString(6))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccPanosBgpPeerGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBgpPeerGroupConfig(vr, name, group.TypeIbgp, group.NextHopOriginal, "", true, false, true, false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosBgpPeerGroupExists("panos_bgp_peer_group.test", &o),
+					testAccCheckPanosBgpPeerGroupAttributes(&o, group.TypeIbgp, group.NextHopOriginal, "", true, false, true, false),
+				),
+			},
+			{
+				Config: testAccBgpPeerGroupConfig(vr, name, group.TypeEbgp, group.NextHopResolve, group.NextHopOriginal, true, true, true, false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosBgpPeerGroupExists("panos_bgp_peer_group.test", &o),
+					testAccCheckPanosBgpPeerGroupAttributes(&o, group.TypeEbgp, group.NextHopResolve, group.NextHopOriginal, true, true, true, false),
+				),
+			},
+			{
+				Config: testAccBgpPeerGroupConfig(vr, name, group.TypeEbgp, group.NextHopUseSelf, group.NextHopUsePeer, false, false, false, true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosBgpPeerGroupExists("panos_bgp_peer_group.test", &o),
+					testAccCheckPanosBgpPeerGroupAttributes(&o, group.TypeEbgp, group.NextHopUseSelf, group.NextHopUsePeer, false, false, false, true),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckPanosBgpPeerGroupExists(n string, o *group.Entry) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Object label ID is not set")
+		}
+
+		fw := testAccProvider.Meta().(*pango.Firewall)
+		vr, name := parseBgpPeerGroupId(rs.Primary.ID)
+		v, err := fw.Network.BgpPeerGroup.Get(vr, name)
+		if err != nil {
+			return fmt.Errorf("Error in get: %s", err)
+		}
+
+		*o = v
+
+		return nil
+	}
+}
+
+func testAccCheckPanosBgpPeerGroupAttributes(o *group.Entry, typ, enh, inh string, en, acap, srwsi, rpa bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if o.Type != typ {
+			return fmt.Errorf("Type is %q, expected %q", o.Type, typ)
+		}
+
+		if o.ExportNextHop != enh {
+			return fmt.Errorf("Export next hop is %q, expected %q", o.ExportNextHop, enh)
+		}
+
+		if o.ImportNextHop != inh {
+			return fmt.Errorf("Import next hop is %q, expected %q", o.ImportNextHop, inh)
+		}
+
+		if o.Enable != en {
+			return fmt.Errorf("Enable is %t, not %t", o.Enable, en)
+		}
+
+		if o.AggregatedConfedAsPath != acap {
+			return fmt.Errorf("Aggregated confed AS path is %t, expected %t", o.AggregatedConfedAsPath, acap)
+		}
+
+		if o.SoftResetWithStoredInfo != srwsi {
+			return fmt.Errorf("Soft reset with stored info is %t, expected %t", o.SoftResetWithStoredInfo, srwsi)
+		}
+
+		if o.RemovePrivateAs != rpa {
+			return fmt.Errorf("Remove private AS is %t, expected %t", o.RemovePrivateAs, rpa)
+		}
+
+		return nil
+	}
+}
+
+func testAccPanosBgpPeerGroupDestroy(s *terraform.State) error {
+	fw := testAccProvider.Meta().(*pango.Firewall)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "panos_bgp_peer_group" {
+			continue
+		}
+
+		if rs.Primary.ID != "" {
+			vr, name := parseBgpPeerGroupId(rs.Primary.ID)
+			_, err := fw.Network.BgpPeerGroup.Get(vr, name)
+			if err == nil {
+				return fmt.Errorf("Object %q still exists", rs.Primary.ID)
+			}
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func testAccBgpPeerGroupConfig(vr, name, typ, enh, inh string, en, acap, srwsi, rpa bool) string {
+	return fmt.Sprintf(`
+resource "panos_ethernet_interface" "e1" {
+    name = "ethernet1/1"
+    vsys = "vsys1"
+    mode = "layer3"
+}
+
+resource "panos_virtual_router" "vr" {
+    name = %q
+    interfaces = ["${panos_ethernet_interface.e1.name}"]
+}
+
+resource "panos_bgp" "conf" {
+    virtual_router = "${panos_virtual_router.vr.name}"
+    router_id = "5.5.5.5"
+    as_number = "42"
+    enable = false
+}
+
+resource "panos_bgp_peer_group" "test" {
+    virtual_router = "${panos_bgp.conf.virtual_router}"
+    name = %q
+    type = %q
+    export_next_hop = %q
+    import_next_hop = %q
+    enable = %t
+    aggregated_confed_as_path = %t
+    soft_reset_with_stored_info = %t
+    remove_private_as = %t
+}
+`, vr, name, typ, enh, inh, en, acap, srwsi, rpa)
+}
---
+++ panos/resource_panorama_bgp_peer_group.go
@@ -0,0 +1,111 @@
+package panos
+
+import (
+	"strings"
+
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/netw/routing/protocol/bgp/peer/group"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourcePanoramaBgpPeerGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: createPanoramaBgpPeerGroup,
+		Read:   readPanoramaBgpPeerGroup,
+		Update: updatePanoramaBgpPeerGroup,
+		Delete: deletePanoramaBgpPeerGroup,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: bgpPeerGroupSchema(true),
+	}
+}
+
+func parsePanoramaBgpPeerGroup(d *schema.ResourceData) (string, string, string, group.Entry) {
+	tmpl := d.Get("template").(string)
+	ts := d.Get("template_stack").(string)
+
+	vr, o := parseBgpPeerGroup(d)
+
+	return tmpl, ts, vr, o
+}
+
+func parsePanoramaBgpPeerGroupId(v string) (string, string, string, string) {
+	t := strings.Split(v, IdSeparator)
+	return t[0], t[1], t[2], t[3]
+}
+
+func buildPanoramaBgpPeerGroupId(a, b, c, d string) string {
+	return strings.Join([]string{a, b, c, d}, IdSeparator)
+}
+
+func createPanoramaBgpPeerGroup(d *schema.ResourceData, meta interface{}) error {
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, o := parsePanoramaBgpPeerGroup(d)
+
+	if err := pano.Network.BgpPeerGroup.Set(tmpl, ts, vr, o); err != nil {
+		return err
+	}
+
+	d.SetId(buildPanoramaBgpPeerGroupId(tmpl, ts, vr, o.Name))
+	return readPanoramaBgpPeerGroup(d, meta)
+}
+
+func readPanoramaBgpPeerGroup(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, name := parsePanoramaBgpPeerGroupId(d.Id())
+
+	o, err := pano.Network.BgpPeerGroup.Get(tmpl, ts, vr, name)
+	if err != nil {
+		if isObjectNotFound(err) {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	d.Set("template", tmpl)
+	d.Set("template_stack", ts)
+
+	saveBgpPeerGroup(d, vr, o)
+
+	return nil
+}
+
+func updatePanoramaBgpPeerGroup(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, o := parsePanoramaBgpPeerGroup(d)
+
+	lo, err := pano.Network.BgpPeerGroup.Get(tmpl, ts, vr, o.Name)
+	if err != nil {
+		return err
+	}
+	lo.Copy(o)
+	if err = pano.Network.BgpPeerGroup.Edit(tmpl, ts, vr, lo); err != nil {
+		return err
+	}
+
+	return readPanoramaBgpPeerGroup(d, meta)
+}
+
+func deletePanoramaBgpPeerGroup(d *schema.ResourceData, meta interface{}) error {
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, name := parsePanoramaBgpPeerGroupId(d.Id())
+
+	err := pano.Network.BgpPeerGroup.Delete(tmpl, ts, vr, name)
+	if err != nil {
+		if !isObjectNotFound(err) {
+			return err
+		}
+	}
+
+	d.SetId("")
+	return nil
+}
