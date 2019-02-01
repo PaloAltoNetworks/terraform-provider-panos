17780d743f73b4146d2720932464ce2e041af868
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-02-01 15:26:36 -0800

    Fixes #130 - bgp redist rules

---
+++ panos/resource_panorama_bgp_redist_rule.go
@@ -0,0 +1,110 @@
+package panos
+
+import (
+	"strings"
+
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/netw/routing/protocol/bgp/redist"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourcePanoramaBgpRedistRule() *schema.Resource {
+	return &schema.Resource{
+		Create: createPanoramaBgpRedistRule,
+		Read:   readPanoramaBgpRedistRule,
+		Update: updatePanoramaBgpRedistRule,
+		Delete: deletePanoramaBgpRedistRule,
+
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: bgpRedistRuleSchema(true),
+	}
+}
+
+func parsePanoramaBgpRedistRule(d *schema.ResourceData) (string, string, string, redist.Entry) {
+	tmpl := d.Get("template").(string)
+	ts := d.Get("template_stack").(string)
+
+	vr, o := parseBgpRedistRule(d)
+
+	return tmpl, ts, vr, o
+}
+
+func parsePanoramaBgpRedistRuleId(v string) (string, string, string, string) {
+	t := strings.Split(v, IdSeparator)
+	return t[0], t[1], t[2], t[3]
+}
+
+func buildPanoramaBgpRedistRuleId(a, b, c, d string) string {
+	return strings.Join([]string{a, b, c, d}, IdSeparator)
+}
+
+func createPanoramaBgpRedistRule(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, o := parsePanoramaBgpRedistRule(d)
+
+	if err = pano.Network.BgpRedistRule.Set(tmpl, ts, vr, o); err != nil {
+		return err
+	}
+
+	d.SetId(buildPanoramaBgpRedistRuleId(tmpl, ts, vr, o.Name))
+	return readPanoramaBgpRedistRule(d, meta)
+}
+
+func readPanoramaBgpRedistRule(d *schema.ResourceData, meta interface{}) error {
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, name := parsePanoramaBgpRedistRuleId(d.Id())
+
+	o, err := pano.Network.BgpRedistRule.Get(tmpl, ts, vr, name)
+	if err != nil {
+		if isObjectNotFound(err) {
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	d.Set("template", tmpl)
+	d.Set("template_stack", ts)
+
+	saveBgpRedistRule(d, vr, o)
+
+	return nil
+}
+
+func updatePanoramaBgpRedistRule(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, o := parsePanoramaBgpRedistRule(d)
+
+	lo, err := pano.Network.BgpRedistRule.Get(tmpl, ts, vr, o.Name)
+	if err != nil {
+		return err
+	}
+	lo.Copy(o)
+	if err = pano.Network.BgpRedistRule.Edit(tmpl, ts, vr, lo); err != nil {
+		return err
+	}
+
+	return readPanoramaBgpRedistRule(d, meta)
+}
+
+func deletePanoramaBgpRedistRule(d *schema.ResourceData, meta interface{}) error {
+	pano := meta.(*pango.Panorama)
+	tmpl, ts, vr, name := parsePanoramaBgpRedistRuleId(d.Id())
+
+	if err := pano.Network.BgpRedistRule.Delete(tmpl, ts, vr, name); err != nil {
+		if !isObjectNotFound(err) {
+			return err
+		}
+	}
+
+	d.SetId("")
+	return nil
+}
---
+++ panos/resource_panorama_bgp_redist_rule_test.go
@@ -0,0 +1,167 @@
+package panos
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/netw/routing/protocol/bgp/redist"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccPanosPanoramaBgpRedistRule_basic(t *testing.T) {
+	if !testAccIsPanorama {
+		t.Skip(SkipPanoramaAccTest)
+	}
+
+	var o redist.Entry
+	tmpl := fmt.Sprintf("tf%s", acctest.RandString(6))
+	vr := fmt.Sprintf("tf%s", acctest.RandString(6))
+	name := fmt.Sprintf("192.168.%d.0/24", acctest.RandInt()%240+5)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccPanosPanoramaBgpRedistRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPanoramaBgpRedistRuleConfig(tmpl, vr, name, redist.SetOriginIgp, "98", "76", "54:32", 21, 12, false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosPanoramaBgpRedistRuleExists("panos_panorama_bgp_redist_rule.test", &o),
+					testAccCheckPanosPanoramaBgpRedistRuleAttributes(&o, name, redist.SetOriginIgp, "98", "76", "54:32", 21, 12, false),
+				),
+			},
+			{
+				Config: testAccPanoramaBgpRedistRuleConfig(tmpl, vr, name, redist.SetOriginIncomplete, "76", "98", "32:54", 12, 21, true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosPanoramaBgpRedistRuleExists("panos_panorama_bgp_redist_rule.test", &o),
+					testAccCheckPanosPanoramaBgpRedistRuleAttributes(&o, name, redist.SetOriginIncomplete, "76", "98", "32:54", 12, 21, true),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckPanosPanoramaBgpRedistRuleExists(n string, o *redist.Entry) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Object label ID is not set")
+		}
+
+		pano := testAccProvider.Meta().(*pango.Panorama)
+		tmpl, ts, vr, name := parsePanoramaBgpRedistRuleId(rs.Primary.ID)
+		v, err := pano.Network.BgpRedistRule.Get(tmpl, ts, vr, name)
+		if err != nil {
+			return fmt.Errorf("Error in get: %s", err)
+		}
+
+		*o = v
+
+		return nil
+	}
+}
+
+func testAccCheckPanosPanoramaBgpRedistRuleAttributes(o *redist.Entry, name, ori, med, lp, com string, met, sapl int, en bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if o.Name != name {
+			return fmt.Errorf("Name is %q, expected %q", o.Name, name)
+		}
+
+		if o.SetOrigin != ori {
+			return fmt.Errorf("Set origin is %q, not %q", o.SetOrigin, ori)
+		}
+
+		if o.SetMed != med {
+			return fmt.Errorf("Set MED is %q, not %q", o.SetMed, med)
+		}
+
+		if o.SetLocalPreference != lp {
+			return fmt.Errorf("Set local preference is %q, not %q", o.SetLocalPreference, lp)
+		}
+
+		if len(o.SetCommunity) != 1 || o.SetCommunity[0] != com {
+			return fmt.Errorf("Set communities is %#v, not [%q]", o.SetCommunity, com)
+		}
+
+		if o.Metric != met {
+			return fmt.Errorf("Metric is %d, not %d", o.Metric, met)
+		}
+
+		if o.SetAsPathLimit != sapl {
+			return fmt.Errorf("Set AS path limit is %d, not %d", o.SetAsPathLimit, sapl)
+		}
+
+		if o.Enable != en {
+			return fmt.Errorf("Enable is %t, not %t", o.Enable, en)
+		}
+
+		return nil
+	}
+}
+
+func testAccPanosPanoramaBgpRedistRuleDestroy(s *terraform.State) error {
+	pano := testAccProvider.Meta().(*pango.Panorama)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "panos_panorama_bgp_redist_rule" {
+			continue
+		}
+
+		if rs.Primary.ID != "" {
+			tmpl, ts, vr, name := parsePanoramaBgpRedistRuleId(rs.Primary.ID)
+			_, err := pano.Network.BgpRedistRule.Get(tmpl, ts, vr, name)
+			if err == nil {
+				return fmt.Errorf("Object %q still exists", rs.Primary.ID)
+			}
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func testAccPanoramaBgpRedistRuleConfig(tmpl, vr, name, ori, med, lp, com string, met, sapl int, en bool) string {
+	return fmt.Sprintf(`
+data "panos_system_info" "x" {}
+
+resource "panos_panorama_template" "t" {
+    name = %q
+}
+
+resource "panos_panorama_virtual_router" "vr" {
+    template = "${panos_panorama_template.t.name}"
+    name = %q
+}
+
+resource "panos_panorama_bgp" "x" {
+    template = "${panos_panorama_template.t.name}"
+    virtual_router = "${panos_panorama_virtual_router.vr.name}"
+    router_id = "5.5.5.5"
+    as_number = "55"
+    enable = false
+}
+
+resource "panos_panorama_bgp_redist_rule" "test" {
+    template = "${panos_panorama_template.t.name}"
+    virtual_router = "${panos_panorama_bgp.x.virtual_router}"
+    address_family = "ipv4"
+    route_table = "${data.panos_system_info.x.version_major >= 8 ? "unicast" : ""}"
+    name = %q
+    set_origin = %q
+    set_med = %q
+    set_local_preference = %q
+    set_communities = [%q]
+    metric = %d
+    set_as_path_limit = %d
+    enable = %t
+}
+`, tmpl, vr, name, ori, med, lp, com, met, sapl, en)
+}
