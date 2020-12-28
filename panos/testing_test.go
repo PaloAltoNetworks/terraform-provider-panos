67cba93285c3785a73a51250c66cd2d649e8b40b
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-12-28 10:35:59 -0800

    adding testing helper functions

---
+++ panos/testing_test.go
@@ -0,0 +1,29 @@
+package panos
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func checkDataSourceListing(x string) resource.TestCheckFunc {
+	return resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttrSet(
+			fmt.Sprintf("data.%s.test", x),
+			"total",
+		),
+	)
+}
+
+func checkDataSource(x string, keys []string) resource.TestCheckFunc {
+	list := make([]resource.TestCheckFunc, 0, len(keys))
+
+	for _, key := range keys {
+		list = append(list, resource.TestCheckResourceAttrSet(
+			fmt.Sprintf("data.%s.test", x),
+			key,
+		))
+	}
+
+	return resource.ComposeTestCheckFunc(list...)
+}
