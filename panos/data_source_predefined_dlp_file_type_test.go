b46c2c92db18d86d3d8f30e2c68d5a21b75f6925
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-01-20 21:02:36 -0800

    adding acctest for predefined dlp file type

---
+++ panos/data_source_predefined_dlp_file_type_test.go
@@ -0,0 +1,37 @@
+package panos
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccPanosDsPredefinedDlpFileType(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPredefinedDlpFileTypeConfig(),
+				Check: checkDataSource("panos_predefined_dlp_file_type", []string{
+					"total",
+					"file_types.0.name",
+					"file_types.0.properties.0.name",
+					"file_types.0.properties.0.label",
+					"file_types.1.name",
+					"file_types.1.properties.0.name",
+					"file_types.1.properties.0.label",
+					"file_types.2.name",
+					"file_types.2.properties.0.name",
+					"file_types.2.properties.0.label",
+				}),
+			},
+		},
+	})
+}
+
+func testAccPredefinedDlpFileTypeConfig() string {
+	return `
+data "panos_predefined_dlp_file_type" "test" {}
+`
+}
