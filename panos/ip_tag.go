21c3d49d7a202b9f3d775121251982987cf4cc71
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-01-25 08:19:47 -0800

    adding new panos_ip_tag implementation and acctests

---
+++ panos/ip_tag.go
@@ -0,0 +1,262 @@
+package panos
+
+import (
+	"log"
+	"strings"
+
+	"github.com/PaloAltoNetworks/pango"
+	"github.com/PaloAltoNetworks/pango/userid"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+// Data source.
+func dataSourceIpTag() *schema.Resource {
+	return &schema.Resource{
+		Read: readDataSourceIpTag,
+
+		Schema: map[string]*schema.Schema{
+			"vsys": vsysSchema(),
+			"ip": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Optionally filter on just this single IP address",
+			},
+			"tag": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Optionally filter on just this single tag",
+			},
+			"total": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Total number of entries",
+			},
+			"entries": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "List of entry specs",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ip": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The IP address",
+						},
+						"tags": {
+							Type:        schema.TypeSet,
+							Computed:    true,
+							Description: "The IP's tags",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func readDataSourceIpTag(d *schema.ResourceData, meta interface{}) error {
+	fw, err := firewall(meta, "")
+	if err != nil {
+		return err
+	}
+
+	ans, err := fw.UserId.GetIpTags(
+		d.Get("ip").(string),
+		d.Get("tag").(string),
+		d.Get("vsys").(string),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(base64Encode([]interface{}{
+		d.Get("ip").(string), d.Get("tag").(string), d.Get("vsys").(string),
+	}))
+
+	if len(ans) == 0 {
+		d.Set("total", 0)
+		d.Set("entries", nil)
+	} else {
+		d.Set("total", len(ans))
+		list := make([]interface{}, 0, len(ans))
+		for ip, tags := range ans {
+			list = append(list, map[string]interface{}{
+				"ip":   ip,
+				"tags": listAsSet(tags),
+			})
+		}
+
+		if err = d.Set("entries", list); err != nil {
+			log.Printf("[WARN] Error setting 'entries' for %q: %s", d.Id(), err)
+		}
+	}
+
+	return nil
+}
+
+// Resource.
+func resourceIpTag() *schema.Resource {
+	return &schema.Resource{
+		Create: createIpTag,
+		Read:   readIpTag,
+		Delete: deleteIpTag,
+
+		Schema: map[string]*schema.Schema{
+			"vsys": vsysSchema(),
+			"ip": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "IP address to tag",
+				ForceNew:    true,
+			},
+			"tags": {
+				Type:        schema.TypeSet,
+				Required:    true,
+				Description: "Tags",
+				MinItems:    1,
+				ForceNew:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	}
+}
+
+func createIpTag(d *schema.ResourceData, meta interface{}) error {
+	fw, err := firewall(meta, "")
+	if err != nil {
+		return err
+	}
+	vsys := d.Get("vsys").(string)
+	ip := d.Get("ip").(string)
+	tagList := d.Get("tags").(*schema.Set).List()
+
+	cur, err := fw.UserId.GetIpTags(ip, "", vsys)
+	if err != nil {
+		return err
+	}
+	curTags := cur[ip]
+
+	missing := make([]string, 0, len(tagList))
+	for i := range tagList {
+		tag := tagList[i].(string)
+		var found bool
+		for _, x := range curTags {
+			if x == tag {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			missing = append(missing, tag)
+		}
+	}
+
+	if len(missing) > 0 {
+		msg := &userid.Message{
+			TagIps: []userid.TagIp{
+				userid.TagIp{
+					Ip:   ip,
+					Tags: missing,
+				},
+			},
+		}
+
+		if err = fw.UserId.Run(msg, vsys); err != nil {
+			return err
+		}
+	}
+
+	d.SetId(buildIpTagId(vsys, ip, tagList))
+	return readIpTag(d, meta)
+}
+
+func readIpTag(d *schema.ResourceData, meta interface{}) error {
+	fw, err := firewall(meta, "")
+	if err != nil {
+		return err
+	}
+	vsys, ip, tagList := parseIpTagId(d.Id())
+
+	cur, err := fw.UserId.GetIpTags(ip, "", vsys)
+	if err != nil || len(cur) == 0 {
+		d.SetId("")
+		return nil
+	}
+	curTags := cur[ip]
+
+	list := make([]string, 0, len(tagList))
+	for _, tag := range tagList {
+		for _, curTag := range curTags {
+			if tag == curTag {
+				list = append(list, tag)
+				break
+			}
+		}
+	}
+
+	d.Set("ip", ip)
+	if len(list) == 0 {
+		d.Set("tags", nil)
+	} else if err = d.Set("tags", listAsSet(list)); err != nil {
+		log.Printf("[WARN] Error setting 'tags' for %q: %s", d.Id(), err)
+	}
+
+	return nil
+}
+
+func deleteIpTag(d *schema.ResourceData, meta interface{}) error {
+	fw := meta.(*pango.Firewall)
+	vsys, ip, tagList := parseIpTagId(d.Id())
+
+	cur, err := fw.UserId.GetIpTags(ip, "", vsys)
+	if err != nil {
+		return err
+	}
+	curTags := cur[ip]
+
+	list := make([]string, 0, len(tagList))
+	for _, tag := range tagList {
+		for _, curTag := range curTags {
+			if tag == curTag {
+				list = append(list, tag)
+				break
+			}
+		}
+	}
+
+	if len(list) > 0 {
+		msg := &userid.Message{
+			UntagIps: []userid.UntagIp{
+				userid.UntagIp{
+					Ip:   ip,
+					Tags: list,
+				},
+			},
+		}
+
+		if err = fw.UserId.Run(msg, vsys); err != nil {
+			return err
+		}
+	}
+
+	d.SetId("")
+	return nil
+}
+
+// Id functions.
+func buildIpTagId(a, b string, c []interface{}) string {
+	return strings.Join([]string{a, b, base64Encode(c)}, IdSeparator)
+}
+
+func parseIpTagId(v string) (string, string, []string) {
+	t := strings.Split(v, IdSeparator)
+	return t[0], t[1], base64Decode(t[2])
+}
---
+++ panos/ip_tag_test.go
@@ -0,0 +1,177 @@
+package panos
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/PaloAltoNetworks/pango"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+// Data source test.
+func TestAccPanosDsIpTag(t *testing.T) {
+	if !testAccIsFirewall {
+		t.Skip(SkipFirewallAccTest)
+	}
+
+	ip := fmt.Sprintf("10.1.59.%d", acctest.RandInt()%50+50)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDsIpTagConfig(ip),
+				Check: checkDataSource("panos_ip_tag", []string{
+					"ip", "entries.0.ip",
+				}),
+			},
+		},
+	})
+}
+
+func testAccDsIpTagConfig(ip string) string {
+	return fmt.Sprintf(`
+data "panos_ip_tag" "test" {
+    ip = panos_ip_tag.x.ip
+}
+
+resource "panos_ip_tag" "x" {
+    ip = %q
+    tags = [
+        "terraform",
+        "datasource",
+        "acctest",
+    ]
+}
+`, ip)
+}
+
+// Resource test.
+func TestAccPanosIpTag(t *testing.T) {
+	var o map[string][]string
+	ip := fmt.Sprintf("10.1.59.%d", acctest.RandInt()%50+50)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccPanosIpTagDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIpTagConfig(ip, "tag1", "tag2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosIpTagExists("panos_ip_tag.test", &o),
+					testAccCheckPanosIpTagAttributes(&o, ip, "tag1", "tag2"),
+				),
+			},
+			{
+				Config: testAccIpTagConfig(ip, "tag3", "tag2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPanosIpTagExists("panos_ip_tag.test", &o),
+					testAccCheckPanosIpTagAttributes(&o, ip, "tag3", "tag2"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckPanosIpTagExists(n string, o *map[string][]string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Object label ID is not set")
+		}
+
+		var err error
+		var v map[string][]string
+
+		switch con := testAccProvider.Meta().(type) {
+		case *pango.Firewall:
+			vsys, ip, _ := parseIpTagId(rs.Primary.ID)
+			v, err = con.UserId.GetIpTags(ip, "", vsys)
+		}
+
+		if err != nil {
+			return fmt.Errorf("Error in get: %s", err)
+		}
+
+		*o = v
+
+		return nil
+	}
+}
+
+func testAccCheckPanosIpTagAttributes(o *map[string][]string, name, tag1, tag2 string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		var found1 bool
+		var found2 bool
+
+		tags := (*o)[name]
+
+		for _, tag := range tags {
+			if tag == tag1 {
+				found1 = true
+			} else if tag == tag2 {
+				found2 = true
+			}
+			if found1 && found2 {
+				break
+			}
+		}
+
+		if !found1 {
+			return fmt.Errorf("%q not in tags: %#v", tag1, tags)
+		}
+
+		if !found2 {
+			return fmt.Errorf("%q not in tags: %#v", tag2, tags)
+		}
+
+		return nil
+	}
+}
+
+func testAccPanosIpTagDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "panos_ip_tag" {
+			continue
+		}
+
+		if rs.Primary.ID != "" {
+			var err error
+			var ip string
+			var curTags map[string][]string
+
+			switch con := testAccProvider.Meta().(type) {
+			case *pango.Firewall:
+				vsys, ip, _ := parseIpTagId(rs.Primary.ID)
+				curTags, err = con.UserId.GetIpTags(ip, "", vsys)
+				if err != nil {
+					return err
+				}
+			}
+			if len(curTags[ip]) != 0 {
+				return fmt.Errorf("User %q still has tags: %#v", ip, curTags[ip])
+			}
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func testAccIpTagConfig(ip, tag1, tag2 string) string {
+	return fmt.Sprintf(`
+resource "panos_ip_tag" "test" {
+    ip = %q
+    tags = [%q, %q]
+}
+`, ip, tag1, tag2)
+}
