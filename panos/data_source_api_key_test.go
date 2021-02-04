13857a18eb117a5656c864fee3ec13401ced19ab
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-02-04 14:03:28 -0800

    adding api key data source acctest

---
+++ panos/data_source_api_key_test.go
@@ -0,0 +1,28 @@
+package panos
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccPanosApiKey(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccApiKeyConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.panos_api_key.test", "api_key"),
+				),
+			},
+		},
+	})
+}
+
+func testAccApiKeyConfig() string {
+	return `
+data "panos_api_key" "test" {}
+`
+}
