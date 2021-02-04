e2f3089d2089a4e575ca63450d0c664d23461ea8
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-02-04 14:02:42 -0800

    adding api key data source

---
+++ panos/data_source_api_key.go
@@ -0,0 +1,38 @@
+package panos
+
+import (
+	"github.com/PaloAltoNetworks/pango"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceApiKey() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceApiKeyRead,
+
+		Schema: map[string]*schema.Schema{
+			"api_key": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The API key associated with the provider's authentication credentials",
+			},
+		},
+	}
+}
+
+func dataSourceApiKeyRead(d *schema.ResourceData, meta interface{}) error {
+	switch c := meta.(type) {
+	case *pango.Firewall:
+		d.SetId(base64Encode([]interface{}{
+			c.Hostname, c.Username,
+		}))
+		d.Set("api_key", c.ApiKey)
+	case *pango.Panorama:
+		d.SetId(base64Encode([]interface{}{
+			c.Hostname, c.Username,
+		}))
+		d.Set("api_key", c.ApiKey)
+	}
+
+	return nil
+}
