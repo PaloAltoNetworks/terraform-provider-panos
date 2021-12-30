4d394958ea31bcbb0c362c51ab4b11876209abc2
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-12-30 15:49:41 -0800

    tweak anti spyware

--- panos/anti_spyware_security_profile.go
+++ panos/anti_spyware_security_profile.go
@@ -13,7 +13,7 @@ import (
 // Data source (listing).
 func dataSourceAntiSpywareSecurityProfiles() *schema.Resource {
 	s := listingSchema()
-	s["vsys"] = vsysSchema()
+	s["vsys"] = vsysSchema("vsys1")
 	s["device_group"] = deviceGroupSchema()
 
 	return &schema.Resource{
@@ -205,7 +205,7 @@ func deleteAntiSpywareSecurityProfile(d *schema.ResourceData, meta interface{}) 
 // Schema handling.
 func antiSpywareSecurityProfileSchema(isResource bool) map[string]*schema.Schema {
 	ans := map[string]*schema.Schema{
-		"vsys":         vsysSchema(),
+		"vsys":         vsysSchema("vsys1"),
 		"device_group": deviceGroupSchema(),
 		"name": {
 			Type:        schema.TypeString,
