eedf05eebaba0d546d5f5c61ad7881aa12fb2950
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-12-30 15:41:24 -0800

    tweak dynamic user group support

--- panos/dynamic_user_group.go
+++ panos/dynamic_user_group.go
@@ -13,7 +13,7 @@ import (
 // Data source (listing).
 func dataSourceDynamicUserGroups() *schema.Resource {
 	s := map[string]*schema.Schema{
-		"vsys":         vsysSchema(),
+		"vsys":         vsysSchema("vsys1"),
 		"device_group": deviceGroupSchema(),
 	}
 
@@ -211,7 +211,7 @@ func deleteDynamicUserGroup(d *schema.ResourceData, meta interface{}) error {
 func dynamicUserGroupSchema(isResource bool) map[string]*schema.Schema {
 	ans := map[string]*schema.Schema{
 		"device_group": deviceGroupSchema(),
-		"vsys":         vsysSchema(),
+		"vsys":         vsysSchema("vsys1"),
 		"name": {
 			Type:        schema.TypeString,
 			Required:    true,
