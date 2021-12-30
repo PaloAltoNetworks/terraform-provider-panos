a93963b864ad32c04c913e639e2515d51173bcbf
Garfield Freeman <gfreeman@paloaltonetworks.com>
2021-12-30 15:51:37 -0800

    tweak custom data pattern object

--- panos/custom_data_pattern_object.go
+++ panos/custom_data_pattern_object.go
@@ -13,7 +13,7 @@ import (
 // Data source (listing).
 func dataSourceCustomDataPatternObjects() *schema.Resource {
 	s := listingSchema()
-	s["vsys"] = vsysSchema()
+	s["vsys"] = vsysSchema("vsys1")
 	s["device_group"] = deviceGroupSchema()
 
 	return &schema.Resource{
@@ -205,7 +205,7 @@ func deleteCustomDataPatternObject(d *schema.ResourceData, meta interface{}) err
 // Schema handling.
 func customDataPatternObjectSchema(isResource bool) map[string]*schema.Schema {
 	ans := map[string]*schema.Schema{
-		"vsys":         vsysSchema(),
+		"vsys":         vsysSchema("vsys1"),
 		"device_group": deviceGroupSchema(),
 		"name": {
 			Type:        schema.TypeString,
