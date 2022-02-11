50ce4640b60ba8d9ae6413a949393263bdc3c306
Garfield Freeman <gfreeman@paloaltonetworks.com>
2022-02-11 15:28:40 -0800

    feat: Add `panos_address_objects` resource

--- panos/address_object.go
+++ panos/address_object.go
@@ -3,6 +3,7 @@ package panos
 import (
 	"log"
 	"strings"
+	"time"
 
 	"github.com/PaloAltoNetworks/pango"
 	"github.com/PaloAltoNetworks/pango/objs/addr"
@@ -51,7 +52,7 @@ func dataSourceAddressObject() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceAddressObjectRead,
 
-		Schema: addressObjectSchema(false, ""),
+		Schema: addressObjectSchema(false, nil),
 	}
 }
 
@@ -99,7 +100,7 @@ func resourceAddressObject() *schema.Resource {
 			{
 				Version: 0,
 				Type: (&schema.Resource{
-					Schema: addressObjectSchema(true, "device_group"),
+					Schema: addressObjectSchema(true, []string{"device_group"}),
 				}).CoreConfigSchema().ImpliedType(),
 				Upgrade: addressObjectUpgradeV0,
 			},
@@ -109,7 +110,7 @@ func resourceAddressObject() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 
-		Schema: addressObjectSchema(true, ""),
+		Schema: addressObjectSchema(true, nil),
 	}
 }
 
@@ -125,7 +126,7 @@ func resourcePanoramaAddressObject() *schema.Resource {
 			{
 				Version: 0,
 				Type: (&schema.Resource{
-					Schema: addressObjectSchema(true, "vsys"),
+					Schema: addressObjectSchema(true, []string{"vsys"}),
 				}).CoreConfigSchema().ImpliedType(),
 				Upgrade: addressObjectUpgradeV0,
 			},
@@ -135,7 +136,7 @@ func resourcePanoramaAddressObject() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 
-		Schema: addressObjectSchema(true, ""),
+		Schema: addressObjectSchema(true, nil),
 	}
 }
 
@@ -254,8 +255,119 @@ func deleteAddressObject(d *schema.ResourceData, meta interface{}) error {
 	return nil
 }
 
+// Resource (bulk).
+func resourceAddressObjects() *schema.Resource {
+	return &schema.Resource{
+		Create: createUpdateAddressObjects,
+		Read:   readAddressObjects,
+		Update: createUpdateAddressObjects,
+		Delete: deleteAddressObjects,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Read:   schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: addressObjectsSchema(true),
+	}
+}
+
+func createUpdateAddressObjects(d *schema.ResourceData, meta interface{}) error {
+	var err error
+	var prevNames []string
+	objs := loadAddressObjects(d)
+
+	dg := d.Get("device_group").(string)
+	vsys := d.Get("vsys").(string)
+
+	d.Set("device_group", dg)
+	d.Set("vsys", vsys)
+
+	if d.Id() != "" {
+		_, _, prevNames = parseAddressObjectsId(d.Id())
+	}
+
+	id := buildAddressObjectsId(dg, vsys, objs)
+
+	switch con := meta.(type) {
+	case *pango.Firewall:
+		err = con.Objects.Address.ConfigureGroup(vsys, objs, prevNames)
+	case *pango.Panorama:
+		err = con.Objects.Address.ConfigureGroup(dg, objs, prevNames)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(id)
+	return readAddressObjects(d, meta)
+}
+
+func readAddressObjects(d *schema.ResourceData, meta interface{}) error {
+	var err error
+	var objs, list []addr.Entry
+
+	dg, vsys, names := parseAddressObjectsId(d.Id())
+
+	switch con := meta.(type) {
+	case *pango.Firewall:
+		list, err = con.Objects.Address.GetAll(vsys)
+	case *pango.Panorama:
+		list, err = con.Objects.Address.GetAll(dg)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	objs = make([]addr.Entry, 0, len(list))
+	for _, name := range names {
+		for _, x := range list {
+			if x.Name == name {
+				objs = append(objs, x)
+				break
+			}
+		}
+	}
+
+	if len(objs) == 0 {
+		d.SetId("")
+		return nil
+	}
+
+	saveAddressObjects(d, objs)
+	return nil
+}
+
+func deleteAddressObjects(d *schema.ResourceData, meta interface{}) error {
+	var err error
+
+	dg, vsys, names := parseAddressObjectsId(d.Id())
+	ilist := make([]interface{}, 0, len(names))
+	for _, x := range names {
+		ilist = append(ilist, x)
+	}
+
+	switch con := meta.(type) {
+	case *pango.Firewall:
+		err = con.Objects.Address.Delete(vsys, ilist...)
+	case *pango.Panorama:
+		err = con.Objects.Address.Delete(dg, ilist...)
+	}
+
+	if err != nil && !isObjectNotFound(err) {
+		return err
+	}
+
+	d.SetId("")
+	return nil
+}
+
 // Schema handling.
-func addressObjectSchema(isResource bool, rmKey string) map[string]*schema.Schema {
+func addressObjectSchema(isResource bool, rmKeys []string) map[string]*schema.Schema {
 	ans := map[string]*schema.Schema{
 		"device_group": deviceGroupSchema(),
 		"vsys":         vsysSchema("vsys1"),
@@ -285,8 +397,30 @@ func addressObjectSchema(isResource bool, rmKey string) map[string]*schema.Schem
 		computed(ans, "", []string{"vsys", "device_group", "name"})
 	}
 
-	if rmKey != "" {
+	for _, rmKey := range rmKeys {
 		delete(ans, rmKey)
+	}
+
+	return ans
+}
+
+func addressObjectsSchema(isResource bool) map[string]*schema.Schema {
+	ans := map[string]*schema.Schema{
+		"device_group": deviceGroupSchema(),
+		"vsys":         vsysSchema("vsys1"),
+		"object": {
+			Type:        schema.TypeSet,
+			Description: "An address object spec.",
+			MinItems:    1,
+			Required:    true,
+			Elem: &schema.Resource{
+				Schema: addressObjectSchema(true, []string{"device_group", "vsys"}),
+			},
+		},
+	}
+
+	if !isResource {
+		computed(ans, "", []string{"vsys", "device_group"})
 	}
 
 	return ans
@@ -312,6 +446,57 @@ func saveAddressObject(d *schema.ResourceData, o addr.Entry) {
 	}
 }
 
+func loadAddressObjects(d *schema.ResourceData) []addr.Entry {
+	olist := d.Get("object").(*schema.Set).List()
+	ans := make([]addr.Entry, 0, len(olist))
+
+	for i := range olist {
+		elm := olist[i].(map[string]interface{})
+		ans = append(ans, addr.Entry{
+			Name:        elm["name"].(string),
+			Value:       elm["value"].(string),
+			Type:        elm["type"].(string),
+			Description: elm["description"].(string),
+			Tags:        asStringList(elm["tags"].([]interface{})),
+		})
+	}
+
+	return ans
+}
+
+func saveAddressObjects(d *schema.ResourceData, objs []addr.Entry) {
+	items := make([]interface{}, 0, len(objs))
+
+	for _, x := range objs {
+		var tlist []interface{}
+		if len(x.Tags) > 0 {
+			tlist = make([]interface{}, 0, len(x.Tags))
+			for _, x := range x.Tags {
+				tlist = append(tlist, x)
+			}
+		}
+
+		items = append(items, map[string]interface{}{
+			"name":        x.Name,
+			"type":        x.Type,
+			"value":       x.Value,
+			"description": x.Description,
+			"tags":        tlist,
+		})
+	}
+
+	s := schema.NewSet(
+		schema.HashResource(
+			addressObjectsSchema(true)["object"].Elem.(*schema.Resource),
+		),
+		items,
+	)
+
+	if err := d.Set("object", s); err != nil {
+		log.Printf("[WARN] Error setting 'object' for %q: %s", d.Id(), err)
+	}
+}
+
 // Id functions.
 func parseAddressObjectId(v string) (string, string) {
 	t := strings.Split(v, IdSeparator)
@@ -320,4 +505,18 @@ func parseAddressObjectId(v string) (string, string) {
 
 func buildAddressObjectId(a, b string) string {
 	return strings.Join([]string{a, b}, IdSeparator)
+}
+
+func buildAddressObjectsId(a, b string, c []addr.Entry) string {
+	list := make([]string, 0, len(c))
+	for _, x := range c {
+		list = append(list, x.Name)
+	}
+
+	return strings.Join([]string{a, b, base64Encode(list)}, IdSeparator)
+}
+
+func parseAddressObjectsId(v string) (string, string, []string) {
+	t := strings.Split(v, IdSeparator)
+	return t[0], t[1], base64Decode(t[2])
 }
