cbeb8e6c104d9f1f56b4d14d198c20b35afc62cb
Nick Culver <nicholas.culver@genmills.com>
2023-12-05 09:36:57 -0600

    removal of helper/hashcode dependency

--- panos/hashes.go
+++ panos/hashes.go
@@ -3,15 +3,43 @@ package panos
 import (
 	"bytes"
 	"fmt"
-
-	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
 )
+
+// From https://developer.hashicorp.com/terraform/plugin/sdkv2/guides/v2-upgrade-guide#removal-of-helper-hashcode-package
+
+// String hashes a string to a unique hashcode.
+//
+// crc32 returns a uint32, but for our use we need
+// and non negative integer. Here we cast to an integer
+// and invert it if the result is negative.
+func String(s string) int {
+	v := int(crc32.ChecksumIEEE([]byte(s)))
+	if v >= 0 {
+		return v
+	}
+	if -v >= 0 {
+		return -v
+	}
+	// v == MinInt
+	return 0
+}
+
+// Strings hashes a list of strings to a unique hashcode.
+func Strings(strings []string) string {
+	var buf bytes.Buffer
+
+	for _, s := range strings {
+		buf.WriteString(fmt.Sprintf("%s-", s))
+	}
+
+	return fmt.Sprintf("%d", String(buf.String()))
+}
 
 func resourceMatchAddressPrefixHash(v interface{}) int {
 	var buf bytes.Buffer
 	m := v.(map[string]interface{})
 	buf.WriteString(fmt.Sprintf("%s%t", m["prefix"].(string), m["exact"].(bool)))
-	return hashcode.String(buf.String())
+	return String(buf.String())
 }
 
 func resourceTargetHash(v interface{}) int {
@@ -22,5 +50,5 @@ func resourceTargetHash(v interface{}) int {
 	for i := range vl {
 		buf.WriteString(vl[i].(string))
 	}
-	return hashcode.String(buf.String())
+	return String(buf.String())
 }
