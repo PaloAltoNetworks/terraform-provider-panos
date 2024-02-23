6e261375ca9afeed43ce1c83e53da310689219c1
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2024-02-23 15:59:47 +0100

    correcting docstring

--- examples/data-sources/panos_tfid/data-source.tf
+++ examples/data-sources/panos_tfid/data-source.tf
@@ -2,7 +2,7 @@
 # by a given resource, but it will be compatible and a valid ID for
 # doing state imports.
 
-# Example of how to create the ID for panos_address_object named
+# Example of how to create the ID for panos_nested_address_object named
 # "foo" in vsys1.
 #
 # All variables should be specified for a given location, default value or not.
