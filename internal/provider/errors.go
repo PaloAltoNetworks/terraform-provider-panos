c8a87fd7278483ead87e62577f98b05c03e43909
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2024-03-05 22:28:28 +0100

    adding new error for when the provider is in inspection mode

--- internal/provider/errors.go
+++ internal/provider/errors.go
@@ -1,8 +1,10 @@
 package provider
 
 import (
-    "github.com/PaloAltoNetworks/pango/errors"
+	"github.com/PaloAltoNetworks/pango/errors"
 )
+
+var InspectionModeError = "Resources are unavailable when the provider is in inspection mode.  Resources are only available in API mode."
 
 func IsObjectNotFound(e error) bool {
 	e2, ok := e.(errors.Panos)
