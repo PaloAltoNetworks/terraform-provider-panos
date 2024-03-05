0ddbf19aa37e58cfa00e1701c0ca04af3fd12bbd
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2024-03-05 22:35:09 +0100

    adding inspection mode example to provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,7 +1,14 @@
+# Traditional provider example.
 provider "panos" {
   hostname = "10.1.1.1"
   username = "admin"
   password = "secret"
+}
+
+# Local inspection mode provider example.
+provider "panos" {
+    config_file = file("/tmp/candidate-config.xml")
+    panos_version = "10.2.0"
 }
 
 terraform {
